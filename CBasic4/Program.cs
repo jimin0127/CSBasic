bbc5925a49438a895ae60c3d53195f731cb979b7
jimin0127 <ckdgh4662@naver.com>
2021-05-26 09:42:41 +0900

    #25 클래스 개요, 사용 생성

--- CBasic4/Program.cs
+++ CBasic4/Program.cs
@@ -6,10 +6,84 @@ using System.Threading.Tasks;
 
 namespace CBasic4
 {
+
+    class Car
+    {
+        int carNumber;
+        DateTime inTime;
+        DateTime outTime;
+
+        public void SetInTime()
+        {
+            this.inTime = DateTime.Now;
+        }
+
+        public void SetOutTime()
+        {
+            this.outTime = DateTime.Now;
+        }
+
+    }
+
+
     class Program
     {
+        /*class Math
+        {
+           클래스 이름 충돌  
+        }*/
         static void Main(string[] args)
         {
+            Car[] cars = new Car[10];
+
+            Random random = new Random();
+            Console.WriteLine(random.Next());
+            Console.WriteLine(random.Next(100)); //0부터 100미만 (0 ~ 99)
+            Console.WriteLine(random.Next(10, 100)); //
+            
+            Console.WriteLine(random.NextDouble());
+            Console.WriteLine(random.NextDouble());
+            Console.WriteLine(random.NextDouble() * 100);
+
+            List<int> list = new List<int>();
+            list.Add(52);
+            list.Add(15);
+            list.Add(8);
+            list.Add(89);
+
+            foreach(var item in list)
+            {
+                Console.WriteLine("Count : " + list.Count + "\titem: " + item);
+            }
+            list.Remove(52);
+            foreach (var item in list)
+            {
+                Console.WriteLine("Count : " + list.Count + "\titem: " + item);
+                //list.Remove(item);
+            }
+
+            Console.WriteLine(Math.Abs(-52288));
+            Console.WriteLine(Math.Ceiling(53.273));
+            Console.WriteLine(Math.Floor(53.273));
+            Console.WriteLine(Math.Max(52, 273));
+            Console.WriteLine(Math.Min(52, 273));
+            Console.WriteLine(Math.Round(52.273));
+
+            Product product = new Product();
+
+        }
+    }
+
+
+    class FirstClass
+    {
+
+    }
+    class SecondClass
+    {
+        class ChildClass
+        {
+
         }
     }
 }
