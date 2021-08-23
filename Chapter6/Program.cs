308d352df402e1cc979bb0a020cde2dacb456cde
jimin0127 <ckdgh4662@naver.com>
2021-08-23 16:09:38 +0900

    쉐도잉, 하이딩, 오버라이딩, 추상 클래스, 추상 메서드 예제

--- Chapter6/Program.cs
+++ Chapter6/Program.cs
@@ -6,6 +6,16 @@ using System.Threading.Tasks;
 
 namespace Chapter6
 {
+    abstract class AbstractParent
+    {
+        public abstract void Test();
+    }
+
+    class AbstractChild : AbstractParent
+    {
+        public override void Test() { }
+    }
+
     class Animal
     {
         public int Age { get; set; }
@@ -21,16 +31,62 @@ namespace Chapter6
 
     }
 
+    class Parent
+    {
+        public int variable = 273;
+        /*public void Method()
+        {
+            Console.WriteLine("부모의 메소드");
+        }*/
+        public virtual void Method() // 오버라이드 할 수 있도록 virtual
+        {
+            Console.WriteLine("부모의 메소드");
+        }
+
+    }
+    class Child: Parent
+    {
+        public string variable = "shadowing";
+        public override void Method() // 오버라이드(virtual 명시된 메서드만)
+        {
+            Console.WriteLine("자식의 메서드");
+        }
+        /*public new void Method() // 명시 하이딩
+        {
+            Console.WriteLine("자식의 메소드");
+        }*/
+    }
+
+
     class Cat : Animal
     {
         public void Meow() { Console.WriteLine("냥냥 웁니다."); }
-        
+
     }
 
-    class Program
+
+        class Program
     {
         static void Main(string[] args)
         {
+            Child child = new Child();
+            child.Method();
+            ((Parent)child).Method();
+            Console.WriteLine((new Child()).variable);
+
+            Console.WriteLine(((Parent)(new Child())).variable);
+            List<Animal> Animals = new List<Animal>()
+            {
+                new Dog(), new Cat(), new Cat(), new Dog(),
+                new Dog(), new Cat(), new Dog(), new Cat()
+            };
+            
+            foreach(var item in Animals)
+            {
+                item.Eat();
+                item.Sleep();
+                ((Cat)item).Meow();
+            }
         }
     }
 }
